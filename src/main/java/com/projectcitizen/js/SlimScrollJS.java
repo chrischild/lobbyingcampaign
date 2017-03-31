c21df3a20511b7312673c1343dd174ad7db4e665
chrischild <cjwchild@gmail.com>
2017-03-30 23:43:30 -0700

    css and js classes

---
+++ src/main/java/com/projectcitizen/js/SlimScrollJS.java
@@ -0,0 +1,22 @@
+/*
+ * SlimScrollJS.java Created On: 03/30/2017
+ *
+ * Copyright: Project Citizen
+ */
+package com.projectcitizen.js;
+
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+
+/**
+ * @author Chris
+ *
+ */
+public class SlimScrollJS extends JavaScriptResourceReference {
+
+    private static final long serialVersionUID = 1816666731316493784L;
+    public static final SlimScrollJS INSTANCE = new SlimScrollJS();
+
+    public SlimScrollJS() {
+        super(SlimScrollJS.class, "jquery.slimscroll.js");
+    }
+}
