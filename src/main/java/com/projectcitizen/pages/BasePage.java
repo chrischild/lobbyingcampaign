031029b361352cd84ae452e82a0caa9e0e053ca2
chrischild <cjwchild@gmail.com>
2017-03-30 23:44:47 -0700

    updated pages

--- src/main/java/com/projectcitizen/pages/BasePage.java
+++ src/main/java/com/projectcitizen/pages/BasePage.java
@@ -1,13 +1,25 @@
+/*
+ * BasePage.java Created On: 03/30/2017
+ *
+ * Copyright: Project Citizen
+ */
 package com.projectcitizen.pages;
 
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.projectcitizen.css.AdmintLTECSS;
 import com.projectcitizen.css.MetisMenu;
-import com.projectcitizen.css.SbAdmin;
+import com.projectcitizen.js.AdminLTEJS;
+import com.projectcitizen.js.SlimScrollJS;
+import com.projectcitizen.navigationpanel.NavigationPanel.Builder;
+import com.projectcitizen.navigationpanel.SideNavigationPanel;
+import com.projectcitizen.pages.page.HomePage;
+import com.projectcitizen.pages.page.user.UserPage;
 
 import de.agilecoders.wicket.core.Bootstrap;
 import de.agilecoders.wicket.core.markup.html.bootstrap.html.ChromeFrameMetaTag;
@@ -21,13 +33,19 @@ public class BasePage extends WebPage {
 
     public BasePage(final PageParameters parameters) {
         super(parameters);
-        
+
         add(new HtmlTag("html"));
 
         add(new MobileViewportMetaTag("viewport"));
         add(new ChromeFrameMetaTag("chrome-frame"));
-        add(new MetaTag("description", Model.of("description"), Model.of("Apache Wicket & Twitter Bootstrap Demo")));
-        add(new MetaTag("author", Model.of("author"), Model.of("Michael Haitz <michael.haitz@agile-coders.de>")));
+        add(new MetaTag("description", Model.of("description"), Model.of("Project Citizen")));
+        add(new MetaTag("author", Model.of("author"), Model.of("Chris Child")));
+
+        Builder builder = new Builder("navigation", getPage(), parameters);
+        builder = builder.addMenuItem("Home", HomePage.class, "fa fa-home")
+                         .addMenuItem("User", UserPage.class, "fa fa-user");
+
+        add(new SideNavigationPanel(builder, getPage()));
     }
 
     @Override
@@ -36,7 +54,10 @@ public class BasePage extends WebPage {
         Bootstrap.renderHead(response);
 
         response.render(CssHeaderItem.forReference(MetisMenu.INSTANCE));
-        response.render(CssHeaderItem.forReference(SbAdmin.INSTANCE));
+        response.render(CssHeaderItem.forReference(AdmintLTECSS.INSTANCE));
         response.render(CssHeaderItem.forReference(FontAwesomeCssReference.instance()));
+
+        response.render(JavaScriptReferenceHeaderItem.forReference(AdminLTEJS.INSTANCE));
+        response.render(JavaScriptReferenceHeaderItem.forReference(SlimScrollJS.INSTANCE));
     }
 }
--- src/main/java/com/projectcitizen/pages/page/HomePage.java
+++ src/main/java/com/projectcitizen/pages/page/HomePage.java
@@ -1,20 +1,12 @@
-/**
- * 
+/*
+ * HomePage.java Created On: 03/30/2017
+ *
+ * Copyright: Project Citizen
  */
 package com.projectcitizen.pages.page;
 
-import org.apache.wicket.markup.html.form.EmailTextField;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.google.inject.Inject;
-import com.projectcitizen.dao.UserDao;
-import com.projectcitizen.entities.User;
 import com.projectcitizen.pages.BasePage;
 
 /**
@@ -25,49 +17,7 @@ public class HomePage extends BasePage {
 
     private static final long serialVersionUID = -1376073508646701517L;
 
-    @Inject
-    private UserDao userDao;
-
     public HomePage(PageParameters parameters) {
         super(parameters);
-
-        User user = new User();
-        setDefaultModel(Model.of(user));
-
-        Form<User> form = new Form<User>("form", new CompoundPropertyModel<User>(user)) {
-            private static final long serialVersionUID = 1L;
-
-            /*
-             * (non-Javadoc)
-             * @see org.apache.wicket.markup.html.form.Form#onSubmit()
-             */
-            @Override
-            protected void onSubmit() {
-                super.onSubmit();
-                User user = (User) getDefaultModelObject();
-                user.setSalt("blah");
-
-                userDao.insertUpdateUser(user);
-            }
-
-        };
-
-        form.add(new TextField<String>("username", new PropertyModel<String>(user, "username"))
-            .setRequired(Boolean.TRUE).setLabel(Model.of("Username")));
-
-        form.add(new PasswordTextField("password", new PropertyModel<String>(user, "password"))
-            .setRequired(Boolean.TRUE).setLabel(Model.of("Password")));
-
-        form.add(new TextField<String>("name", new PropertyModel<String>(user, "name")).setRequired(Boolean.TRUE)
-            .setLabel(Model.of("Name")));
-
-        form.add(new TextField<String>("address", new PropertyModel<String>(user, "address")).setRequired(Boolean.TRUE)
-            .setLabel(Model.of("Address")));
-
-        form.add(new EmailTextField("email", new PropertyModel<String>(user, "email")).setRequired(Boolean.TRUE)
-            .setLabel(Model.of("Email")));
-
-        add(form);
-
     }
 }
